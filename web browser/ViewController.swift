37ad441f72e6574a9e355a997bd0455ed48d92c4
Aditya Vyavahare <vvaditya23@icloud.com>
2023-11-19 21:42:44 +0530

    Initial Commit

---
+++ web browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  web browser
+//
+//  Created by ヴィヤヴャハレ・アディティヤ on 19/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
